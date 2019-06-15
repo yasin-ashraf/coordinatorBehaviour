97b696716840c9609d5145d43edb1a07670cdc20
Yasin Ashraf <yasin.asharf@gmail.com>
2019-06-15 12:56:56 +0530

    chip group and appbar

--- app/src/main/java/com/yasin/behaviourdemo/MainActivity.kt
+++ app/src/main/java/com/yasin/behaviourdemo/MainActivity.kt
@@ -5,35 +5,41 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ChipGroup.OnCheckedChangeListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        init()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun init() {
+        nav_chips.setOnCheckedChangeListener(this)
+        chip_car.isChecked = true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+
+    override fun onCheckedChanged(p0: ChipGroup?, p1: Int) {
+        when(p1){
+            R.id.chip_car -> {
+                nav_chips.checkedChipId
+            }
+            R.id.chip_bus -> {
+
+            }
+            R.id.chip_walk -> {
+
+            }
+            R.id.chip_rail ->{
+
+            }
         }
     }
 }
