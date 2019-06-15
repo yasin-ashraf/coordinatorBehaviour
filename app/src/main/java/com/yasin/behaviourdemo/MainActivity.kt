57734db4000844c5e9e49571488a81e6af1e8374
Yasin Ashraf <yasin.asharf@gmail.com>
2019-06-15 15:12:22 +0530

    recycler item minimum viable

--- app/src/main/java/com/yasin/behaviourdemo/MainActivity.kt
+++ app/src/main/java/com/yasin/behaviourdemo/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.yasin.behaviourdemo
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import androidx.core.content.ContextCompat
-import com.google.android.material.chip.Chip
+import android.view.View
+import android.view.ViewGroup
 import com.google.android.material.chip.ChipGroup
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,9 +18,40 @@ class MainActivity : AppCompatActivity(), ChipGroup.OnCheckedChangeListener {
     }
 
     private fun init() {
+        applyWindowInsets()
         nav_chips.setOnCheckedChangeListener(this)
         chip_car.isChecked = true
     }
+
+    private fun applyWindowInsets() {
+        coordinator.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+
+        coordinator.setOnApplyWindowInsetsListener { view, windowInsets ->
+
+            val lpActionBar = appBar?.layoutParams as ViewGroup.MarginLayoutParams
+            lpActionBar.topMargin += windowInsets.systemWindowInsetTop
+            lpActionBar.leftMargin += windowInsets.systemWindowInsetLeft
+            lpActionBar.rightMargin += windowInsets.systemWindowInsetRight
+            appBar.layoutParams = lpActionBar
+
+            /*val lpBottomNavigation = bottom_navigation?.layoutParams as ViewGroup.MarginLayoutParams
+            lpBottomNavigation.bottomMargin += windowInsets.systemWindowInsetBottom
+            lpBottomNavigation.leftMargin += windowInsets.systemWindowInsetLeft
+            lpBottomNavigation.rightMargin += windowInsets.systemWindowInsetRight
+            bottom_navigation.layoutParams = lpBottomNavigation
+
+            val lpViewPagerIndicator = committee_card_indicator?.layoutParams as ViewGroup.MarginLayoutParams
+            lpViewPagerIndicator.bottomMargin = lpViewPagerIndicator.bottomMargin + windowInsets.systemWindowInsetBottom + lpBottomNavigation.bottomMargin + bottom_navigation.height
+            committee_card_indicator.layoutParams = lpViewPagerIndicator*/
+
+            // clear this listener so insets aren't re-applied
+            coordinator.setOnApplyWindowInsetsListener(null)
+            windowInsets.consumeSystemWindowInsets()
+        }
+    }
+
 
 
     override fun onCheckedChanged(p0: ChipGroup?, p1: Int) {
